f2b0171b32370969f21cd48523521aa7cd0110ab
Misato <me@mirror-kt.dev>
2025-08-09 12:33:34 +0900

    👷 Upload pdf to artifact

--- scripts/export-to-pdf.ts
+++ scripts/export-to-pdf.ts
@@ -1,21 +1,48 @@
 import { exec } from "node:child_process";
-import { chromium } from "playwright-chromium";
+import { chromium } from "playwright";
 
-const devServer = exec("yarn dev --port 9000");
-if (devServer.stdout) {
-  for await (const line of devServer.stdout) {
-    if (line.toString().includes("Local:   http://localhost:9000")) {
-      break;
-    }
-  }
+console.log("Starting dev server...");
+const devServer = exec("yarn run dev");
+const hostRegex = /(http:\/\/localhost:\d+)/;
+let host: string | undefined;
+if (!devServer.stdout) {
+  devServer.kill();
+  throw Error("Dev server cannot started");
 }
 
+const stripAnsiCodes = (str: string): string =>
+  str.replace(
+    // biome-ignore lint/suspicious/noControlCharactersInRegex: for strip ansi codes
+    /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+    "",
+  );
+for await (const line of devServer.stdout) {
+  console.log(line);
+  const result = stripAnsiCodes(line.toString()).match(hostRegex);
+  if (result) {
+    host = result[1];
+    break;
+  }
+}
+if (!host) {
+  devServer.kill();
+  throw Error("Cannot get host from stdout");
+}
+
+console.log("exporting to pdf...");
 const browser = await chromium.launch();
-const page = await browser.newPage();
-await page.goto("http://localhost:9000");
+const context = await browser.newContext();
+const page = await context.newPage();
+page.on("console", (message) => console.log(message));
+await page.goto(host);
 
 await page.pdf({ path: "resume.pdf", format: "A4" });
 
-await page.close();
-await browser.close();
+console.log("killing dev server...");
 devServer.kill();
+console.log("closing context...");
+await context.close();
+console.log("closed browser...");
+await browser.close();
+
+process.exit(0);
